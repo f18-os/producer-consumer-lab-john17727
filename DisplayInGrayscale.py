f165a7a2a72acf7da0beeec51d4cc4ee9ab9283b
john17727 <justanotherone17727@gmail.com>
2018-11-14 17:07:17 -0700

    Program extracts, converts, and displays frames in threads and concurrently. Some timings may be off.

--- DisplayInGrayscale.py
+++ DisplayInGrayscale.py
@@ -5,11 +5,12 @@ import cv2
 import numpy as np
 import base64
 import queue
+import time
 
 def extractFrames(fileName, outputBuffer):
     # Initialize frame count 
     count = 0
-
+    
     # open video file
     vidcap = cv2.VideoCapture(fileName)
 
@@ -25,89 +26,99 @@ def extractFrames(fileName, outputBuffer):
         jpgAsText = base64.b64encode(jpgImage)
 
         # add the frame to the buffer
-        outputBuffer.put(jpgAsText)
+        if not outputBuffer.full():
+            outputBuffer.put(jpgAsText)
+            time.sleep(.012)
        
         success,image = vidcap.read()
         print('Reading frame {} {}'.format(count, success))
         count += 1
 
     print("Frame extraction complete")
+    return
 
 def convertFrames(inputBuffer, outputBuffer):
     count = 0
+    while True:
+        while not inputBuffer.empty():
+            # get the next frame
+            frameAsText = inputBuffer.get()
 
-    while not inputBuffer.empty():
-        # get the next frame
-        frameAsText = inputBuffer.get()
+            # decode the frame 
+            jpgRawImage = base64.b64decode(frameAsText)
 
-        # decode the frame 
-        jpgRawImage = base64.b64decode(frameAsText)
+            # convert the raw frame to a numpy array
+            jpgImage = np.asarray(bytearray(jpgRawImage), dtype=np.uint8)
 
-        # convert the raw frame to a numpy array
-        jpgImage = np.asarray(bytearray(jpgRawImage), dtype=np.uint8)
+            img = cv2.imdecode( jpgImage ,cv2.IMREAD_UNCHANGED)
 
-        img = cv2.imdecode( jpgImage ,cv2.IMREAD_UNCHANGED)
+            grayscaleFrame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        grayscaleFrame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            success, jpgImage = cv2.imencode('.jpg', grayscaleFrame)
 
-        success, jpgImage = cv2.imencode('.jpg', grayscaleFrame)
+            jpgAsText = base64.b64encode(jpgImage)
+            
+            # add the frame to the buffer
+            outputBuffer.put(jpgAsText)
 
-        jpgAsText = base64.b64encode(jpgImage)
+            print("Converting frame {}".format(count))  
 
-        # add the frame to the buffer
-        outputBuffer.put(jpgAsText)
-
-        print("Converting frame {}".format(count))  
-
-        count += 1
+            count += 1
     print("Frame conversion complete")
+    return
 
 
 
 def displayFrames(inputBuffer):
     # initialize frame count
     count = 0
+    while True:
+        # go through each frame in the buffer until the buffer is empty
+        while not inputBuffer.empty():
+            # get the next frame
+            frameAsText = inputBuffer.get()
 
-    # go through each frame in the buffer until the buffer is empty
-    while not inputBuffer.empty():
-        # get the next frame
-        frameAsText = inputBuffer.get()
+            # decode the frame 
+            jpgRawImage = base64.b64decode(frameAsText)
 
-        # decode the frame 
-        jpgRawImage = base64.b64decode(frameAsText)
+            # convert the raw frame to a numpy array
+            jpgImage = np.asarray(bytearray(jpgRawImage), dtype=np.uint8)
+            
+            # get a jpg encoded frame
+            img = cv2.imdecode( jpgImage ,cv2.IMREAD_UNCHANGED)
 
-        # convert the raw frame to a numpy array
-        jpgImage = np.asarray(bytearray(jpgRawImage), dtype=np.uint8)
-        
-        # get a jpg encoded frame
-        img = cv2.imdecode( jpgImage ,cv2.IMREAD_UNCHANGED)
+            print("Displaying frame {}".format(count))        
 
-        print("Displaying frame {}".format(count))        
+            # display the image in a window called "video" and wait 42ms
+            # before displaying the next frame
+            cv2.imshow("Video", img)
+            if cv2.waitKey(42) and 0xFF == ord("q"):
+                break
 
-        # display the image in a window called "video" and wait 42ms
-        # before displaying the next frame
-        cv2.imshow("Video", img)
-        if cv2.waitKey(42) and 0xFF == ord("q"):
-            break
-
-        count += 1
+            count += 1
 
     print("Finished displaying all frames")
     # cleanup the windows
     cv2.destroyAllWindows()
+    return
 
 # filename of clip to load
 filename = 'clip.mp4'
+BUFF_SIZE = 10
 
 # shared queue  
-extractionQueue = queue.Queue()
-conversionQueue = queue.Queue()
+extractionQueue = queue.Queue(BUFF_SIZE)
+conversionQueue = queue.Queue(BUFF_SIZE)
 
 # extract the frames
-extractFrames(filename,extractionQueue)
+extract = threading.Thread(target=extractFrames, args=(filename, extractionQueue))
 
 # convert the frames
-convertFrames(extractionQueue, conversionQueue)
+convert = threading.Thread(target=convertFrames, args=(extractionQueue, conversionQueue))
 
 # display the frames
-displayFrames(conversionQueue)
+display = threading.Thread(target=displayFrames, args=(conversionQueue,))
+
+extract.start()
+convert.start()
+display.start()
